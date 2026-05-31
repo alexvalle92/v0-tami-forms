af1ef08d604b1c9e171ba3b6849d5cd2b88b0c2c
Tâmili Valle <nutritamilivalle@gmail.com>
2026-05-31 18:48:26 -0300

    atualização layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const _geist = Geist({ subsets: ['latin'] })
+const _geistMono = Geist_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'PlanA - Seu Plano Alimentar Personalizado',
+  description: '',
+  generator: '',
 }
 
 export default function RootLayout({
@@ -19,9 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased`}>
+      <body className="font-sans antialiased">
         {children}
-        <Analytics />
       </body>
     </html>
   )
